2b940eed904f5b1ce4ae565c46bea126505442ce
simpleframework <cknet@126.com>
2014-01-02 16:02:35 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/app/developer/DeveloperWebContext.java
+++ src/main/java/net/simpleframework/app/developer/DeveloperWebContext.java
@@ -2,18 +2,23 @@ package net.simpleframework.app.developer;
 
 import net.simpleframework.ctx.AbstractModuleContext;
 import net.simpleframework.ctx.Module;
+import net.simpleframework.ctx.ModuleFunction;
+import net.simpleframework.ctx.ModuleFunctions;
 import net.simpleframework.mvc.ctx.WebModuleFunction;
 
-public class DeveloperWebContext extends AbstractModuleContext {
+public class DeveloperWebContext extends AbstractModuleContext implements IDeveloperContext {
 
 	@Override
 	protected Module createModule() {
-		return new Module()
-				.setName("simple-module-developer")
-				.setText("developer")
-				.setOrder(10)
-				.setDefaultFunction(
-						new WebModuleFunction(DeveloperPage.class).setName(
-								"simple-module-developer-DeveloperPage").setText("开发者")).setOrder(34);
+		return new Module().setName(MODULE_NAME).setText("developer").setOrder(34)
+				.setDefaultFunction(FUNC_DEVELOPER);
 	}
+
+	@Override
+	protected ModuleFunctions getFunctions() {
+		return ModuleFunctions.of(FUNC_DEVELOPER);
+	}
+
+	public static final ModuleFunction FUNC_DEVELOPER = new WebModuleFunction(DeveloperPage.class)
+			.setName(MODULE_NAME + "-DeveloperPage").setText("开发者");
 }
--- src/main/java/net/simpleframework/app/developer/IDeveloperContext.java
+++ src/main/java/net/simpleframework/app/developer/IDeveloperContext.java
@@ -3,4 +3,6 @@ package net.simpleframework.app.developer;
 import net.simpleframework.ctx.IModuleContext;
 
 public interface IDeveloperContext extends IModuleContext {
+
+	static String MODULE_NAME = "simple-module-developer";
 }
