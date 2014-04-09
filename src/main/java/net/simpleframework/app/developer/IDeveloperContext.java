619ffe3110438be6c00d51dde0c53477755fbf87
simpleframework <cknet@126.com>
2014-04-09 10:32:26 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/app/developer/IDeveloperContext.java
+++ src/main/java/net/simpleframework/app/developer/IDeveloperContext.java
@@ -1,8 +1,9 @@
 package net.simpleframework.app.developer;
 
 import net.simpleframework.ctx.IModuleContext;
+import net.simpleframework.mvc.IMVCContextVar;
 
-public interface IDeveloperContext extends IModuleContext {
+public interface IDeveloperContext extends IModuleContext, IMVCContextVar {
 
 	static String MODULE_NAME = "simple-module-developer";
 }
